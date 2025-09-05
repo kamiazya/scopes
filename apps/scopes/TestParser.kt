1830a54b942b86b1d08f0bf2ebfe3366a5b105f3
Yuki Yamazaki <yuki@kamiazya.tech>
2025-09-05 20:24:55 +0900

    clean: Remove temporary debug files
    Remove debug files that were violating Konsist architecture rules:
    - debug_in_operator.kt (contained getOrNull()!! anti-pattern)
    - test-parser.kt
    - TestInOperator.kt
    - apps/scopes/TestParser.kt
    
    These were temporary files used for debugging the IN operator issue and should not be part of the codebase.

--- apps/scopes/TestParser.kt
+++
@@ -1,9 +0,0 @@
-fun main() {
-    val query = "status IN open,closed"
-    println("Query: $query")
-    println("Position 3 char: '${query[3]}'")
-    println("Character at positions:")
-    for (i in 0 until minOf(10, query.length)) {
-        println("  $i: '${query[i]}'")
-    }
-}
